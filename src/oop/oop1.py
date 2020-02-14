adf15011531292c3585be2fa8e2519d1d8a2e6c8
Victor <arowovici@gmail.com>
2020-02-14 10:47:16 +0100

    Finished up Class Heirachy

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,49 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    pass
+
+
+# Base ==> Vehicle
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+# Base ==> GroundVehicle
+
+
+class Car(GroundVehicle):
+    pass
+
+
+# Base ==> GroundVehicle
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+# Base ==> Vehicle
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+# Base ==> FlightVehicle
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+# Base ==> FlightVehicle
+
+
+class Starship(FlightVehicle):
+    pass
